d3bc1d0da0cf550cc1c7f93dd9c405e9d237d348
TheE1337 <xkcdblag@gmail.com>
2019-07-03 12:01:14 -0700

    no

--- chess-env/chess_env/__init__.py
+++ chess-env/chess_env/__init__.py
@@ -1,5 +1,5 @@
 from gym.envs.registration import register
 register(
-    id='chess-v0',
     entry_point='chess_env.envs:chessEnv',
+    id='chess-v0',
 )
--- chess-env/chess_env/envs/chess_env.py
+++ chess-env/chess_env/envs/chess_env.py
@@ -5,9 +5,18 @@ class chessEnv(gym.Env):
   metadata = {'render.modes': ['human']}
   def __init__(self):
     self.state = chess.Board()
+    self.counter = 0
+	self.done = 0
+	self.add = [0, 0]
+	self.reward = 0.0
 
   def step(self, action):
-    ...
+    if self.state.isCheckmate:
+        if self.state.turn:
+            return 1.0
+        else
+            return -1.0
+
   def reset(self):
     ...
   def render(self, mode='human', close=False):
