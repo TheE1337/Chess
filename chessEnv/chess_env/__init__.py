35ab998c44f9cadabf4c301e5f3ff0af69e265df
TheE1337 <xkcdblag@gmail.com>
2019-07-05 11:50:27 -0700

    Complete restructure. Neat.

--- chessEnv/chess_env/__init__.py
+++
@@ -1,5 +0,0 @@
-from gym.envs.registration import register
-register(
-    entry_point='chessEnv.envs:chessEnv',
-    id='chess_v0',
-)
--- chessEnv/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup
-setup(name='gymchess',
-      version='0.0.1',
-      install_requires=['gym','python-chess','keras','pandas']
-)
